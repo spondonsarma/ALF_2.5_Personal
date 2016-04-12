470abe7d3c24f62060107e02d96e87921e38d242
Florian Goth <CaptainSifff@gmx.de>
2016-04-12 15:00:38 +0200

    commit a working configure.sh.

--- configure.sh
+++ configure.sh
@@ -1,5 +1,5 @@
 export DIR=`pwd`
-export f90=$mpif90
+#export f90=$mpif90
 export f90="gfortran"
 export FL="-c  -w   -r8  -O3   -g  -pg"
 export FL="-c  -w   -r8  -O3  -fbounds-check -ftrace=full"
@@ -8,4 +8,4 @@ export FL="-c  -w   -fdefault-real-8 -O3 "
 export FL="-c  -w   -fdefault-real-8  -fbounds-check -g -fbacktrace -ffpe-trap=zero,invalid"
 export FL="-c  -w  -O3"
 export Libs=${DIR}"/Libraries/"
-export LIB_BLAS_LAPACK=${HOME}"/lib_90/LaPack/lapack.a  "${HOME}"/lib_90/Blas/libblas.a"
+export LIB_BLAS_LAPACK="-llapack -lblas"
