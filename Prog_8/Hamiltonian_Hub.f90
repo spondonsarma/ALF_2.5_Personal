eab884b9733152c458faaa80810971ff525f71f4
Fakher Assaad <assaad@physik.uni-wuerzburg.de>
2016-08-03 09:28:29 +0200

    Included calcualtion of time desplaced correlation functions in  Hamiltonian_Hub.f90 The analysis  programs now produce local time displaced functions

--- Prog_8/Hamiltonian_Hub.f90
+++ Prog_8/Hamiltonian_Hub.f90
@@ -38,8 +38,9 @@
       ! For time displaced
       Integer,                       private :: NobsT
       Complex (Kind=8),              private :: Phase_tau
-      Complex (Kind=8), allocatable, private :: Green_tau(:,:,:,:), Den_tau(:,:,:,:)
-      Complex (Kind=8), allocatable, private :: Green0_tau(:), Den0_tau(:)
+      Complex (Kind=8), allocatable, private :: Green_tau(:,:,:,:), Den_tau(:,:,:,:), SpinZ_tau(:,:,:,:)
+      Complex (Kind=8), allocatable, private :: SpinXY_tau(:,:,:,:)
+      Complex (Kind=8), allocatable, private :: Green0_tau(:), Den0_tau(:), SpinZ0_tau(:), SpinXY0_tau(:)
       
     contains 
 
@@ -309,8 +310,9 @@
 
                
           If (Ltau == 1) then 
-             Allocate ( Green_tau(Latt%N,Ltrot+1,1,1), Den_tau(Latt%N,Ltrot+1,1,1) )
-             Allocate ( Green0_tau(1), Den0_tau(1) )
+             Allocate ( Green_tau(Latt%N,Ltrot+1,1,1), Den_tau   (Latt%N,Ltrot+1,1,1) )
+             Allocate ( SpinZ_tau(Latt%N,Ltrot+1,1,1), SpinXY_tau(Latt%N,Ltrot+1,1,1) )
+             Allocate ( Green0_tau(1), Den0_tau(1), SpinZ0_tau(1), SpinXY0_tau(1) )
           endif
           
         end Subroutine Alloc_obs
@@ -341,8 +343,12 @@
              Phase_tau  = cmplx(0.d0,0.d0)
              Green_tau  = cmplx(0.d0,0.d0)
              Den_tau    = cmplx(0.d0,0.d0)
+             SpinZ_tau  = cmplx(0.d0,0.d0)
+             SpinXY_tau = cmplx(0.d0,0.d0)
              Green0_tau = cmplx(0.d0,0.d0)
              Den0_tau   = cmplx(0.d0,0.d0)
+             SpinZ0_tau  = cmplx(0.d0,0.d0)
+             SpinXY0_tau = cmplx(0.d0,0.d0)
           endif
 
         end Subroutine Init_obs
@@ -500,6 +506,10 @@
              Call Print_bin_tau(Green_tau,Latt,NobsT,Phase_tau, file_pr,dtau       )
              File_pr = "Den_tau"
              Call Print_bin_tau(Den_tau,Latt,NobsT,Phase_tau, file_pr,dtau,Den0_tau)
+             File_pr = "SpinZ_tau"
+             Call Print_bin_tau(SpinZ_tau,Latt,NobsT,Phase_tau, file_pr,dtau,SpinZ0_tau)
+             File_pr = "SpinXY_tau"
+             Call Print_bin_tau(SpinXY_tau,Latt,NobsT,Phase_tau, file_pr,dtau,SpinXY0_tau)
 
           endif
 
@@ -528,7 +538,8 @@
              Phase_tau = Phase_tau + ZS
              NobsT     = NobsT + 1
           endif
-          If ( N_FL == 1 ) then 
+          If ( Model == "Hubbard_SU2"  ) then 
+
              Z =  cmplx(dble(N_SUN),0.d0)
              Do I = 1,Latt%N
                 Do J = 1,Latt%N
@@ -539,14 +550,44 @@
                    Den_tau  (imj,nt+1,1,1) = Den_tau  (imj,nt+1,1,1)  +                           ( &
                         &    Z*Z*(cmplx(1.d0,0.d0) - GTT(I,I,1))*(cmplx(1.d0,0.d0) - G00(J,J,1))  -     &
                         &    Z * GT0(I,J,1)*G0T(J,I,1)                                            ) * ZP * ZS
+
+
+                   SpinZ_tau(imj,nt+1,1,1) = SpinZ_tau(imj,nt+1,1,1) +  &
+                       &      - Z*G0T(J,I,1) * GT0(I,J,1) *ZP*ZS
+
+                   SpinXY_tau(imj,nt+1,1,1) = SpinXY_tau(imj,nt+1,1,1) +  &
+                       &      - Z*G0T(J,I,1) * GT0(I,J,1) *ZP*ZS
                 Enddo
              Enddo
              
              Do I = 1,Latt%N
                 Den0_tau(1) = Den0_tau(1) + Z*(cmplx(1.d0,0.d0) - GTT(I,I,1)) * ZP * ZS
              Enddo
+          Elseif ( Model == "Hubbard_Mz"  ) then 
+             Do I = 1,Latt%N
+                Do J = 1,Latt%N
+                   imj = latt%imj(I,J)
+                   Green_tau(imj,nt+1,1,1) = green_tau(imj,nt+1,1,1)  +   ( GT0(I,J,1) + GT0(I,J,2) ) * ZP* ZS
 
-          Endif
+                   Den_tau(imj,nt+1,1,1)  = Den_tau   (imj,nt+1,1,1)  +  (  &
+                        &    (cmplx(2.0,0.d0) - GTT(I,I,1) - GTT(I,I,2) ) * ( cmplx(2.0,0.d0) - G00(J,J,1) -  G00(J,J,2) ) &
+                        & -  ( G0T(J,I,1) * GT0(I,J,1) + G0T(J,I,2) * GT0(I,J,2) )   )*ZP*ZS     
+
+                   SpinZ_tau(imj,nt+1,1,1) = SpinZ_tau(imj,nt+1,1,1) + ( &
+                       &    (GTT(I,I,1) -  GTT(I,I,2) ) * ( G00(J,J,1)  -  G00(J,J,2) )   &
+                       &  - (G0T(J,I,1) * GT0(I,J,1)  +  G0T(J,I,2) * GT0(I,J,2) )    )*ZP*ZS
+
+                   SpinXY_tau(imj,nt+1,1,1) = SpinXY_tau(imj,nt+1,1,1)  - &
+                        &   (G0T(J,I,1) * GT0(I,J,2)  +  G0T(J,I,2) * GT0(I,J,1))*ZP*ZS
+
+                enddo
+             enddo
+             
+             Do I = 1,Latt%N
+                Den0_tau(1) = Den0_tau(1) + (cmplx(2.d0,0.d0) - GTT(I,I,1) - GTT(I,I,2)) * ZP * ZS
+             Enddo
+          endif
+
         end Subroutine OBSERT
 
 
