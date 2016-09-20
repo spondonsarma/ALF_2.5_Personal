fd8a42c0d31eb504f9d1bf646987c562d254271b
Florian Goth <CaptainSifff@gmx.de>
2016-09-20 16:38:00 +0200

    reset back to Fakhers flags.

--- configure.sh
+++ configure.sh
@@ -1,5 +1,5 @@
 export DIR=`pwd`
 export f90="gfortran"
-export FL="-c -O2 -ftree-vectorize -march=broadwell"
+export FL="-c -O3"
 export Libs=${DIR}"/Libraries/"
 export LIB_BLAS_LAPACK="-llapack -lblas"
