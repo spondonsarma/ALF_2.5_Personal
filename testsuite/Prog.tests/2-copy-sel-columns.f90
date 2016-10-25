6353895b93aac2d892ba060c796b8c99e1b5fcda
Florian Goth <CaptainSifff@gmx.de>
2016-10-25 13:19:43 +0200

    Fix test Prog/2

--- testsuite/Prog.tests/2-copy-sel-columns.f90
+++ testsuite/Prog.tests/2-copy-sel-columns.f90
@@ -1,39 +1,45 @@
 ! compile with
-! gfortran -std=f2003  -I ../../../Libraries/Modules/ -I ../../../Prog_8/ -L ../../../Libraries/Modules/ -L ../../../Prog_8/  main.f90 ../../../Prog_8/Operator.o ../../../Libraries/Modules/modules_90.a -llapack -lblas ../../../Libraries/MyNag/libnag.a 
-
-Program CSC
-
-Use Operator_mod
-
-
-        COMPLEX (KIND=KIND(0.D0)), DIMENSION(3,3) :: Vnew, Vold
-        COMPLEX (KIND=KIND(0.D0)), DIMENSION(5,5) :: mat
-        Integer, DIMENSION(3) :: P
-        Integer :: i,j,n
-        
-        do i=1,3
-            do j=1,5
-                mat(i,j) = CMPLX(i, j, kind(0.D0))
-            enddo
-        P(i) = i
-        enddo
-        
-        call copy_select_columns(Vnew, mat, P, 3, 3)
-        
+! gfortran -std=f2003  -I ../../../Libraries/Modules/ -L ../../../Libraries/Modules/ 2-copy-sel-columns.f90 ../../../Prog_8/Operator.o ../../../Libraries/Modules/modules_90.a -llapack -lblas ../../../Libraries/MyNag/libnag.a
+!
+Program CSR
+!
+      Use Operator_mod
+      Complex (Kind=Kind(0.D0)), Dimension (:, :), Allocatable :: Vnew, &
+     & Vold
+      Complex (Kind=Kind(0.D0)), Dimension (:, :), Allocatable :: mat
+      Integer, Dimension (:), Allocatable :: P
+      Integer :: i, j, n, opn, NDim
+!
+      opn = 3
+      NDim = 5
+      Allocate (Vold(opn, NDim), Vnew(opn, NDim), P(opn), mat(NDim, &
+     & NDim))
+      Do i = 1, NDim
+         Do j = 1, NDim
+            mat (i, j) = CMPLX (i, j, kind(0.D0))
+         End Do
+      End Do
+!
+      Do i = 1, opn
+         P (i) = i
+      End Do
+!
+      Call copy_select_columns (Vnew, mat, P, opn, NDim)
+!
 ! check old version
-    Do n = 1,3
-       Do I = 1,3
-          Vold(n,I) = Mat(P(n),I)
-       Enddo
-    Enddo
-
-    do i = 1,3
-        do j = 1,3
-        if (Vold(i,j) .ne. Vnew(i,j)) then
-        write (*,*) "ERROR"
-        STOP 2
-        endif
-        enddo
-    enddo
-
-end Program CSC
+      Do n = 1, opn
+         Do i = 1, NDim
+            Vold (n, i) = mat (P(n), i)
+         End Do
+      End Do
+!
+      Do i = 1, opn
+         Do j = 1, NDim
+            If (Vold(i, j) .Ne. Vnew(i, j)) Then
+               Write (*,*) "ERROR", Vold (i, j), Vnew (i, j)
+               Stop 2
+            End If
+         End Do
+      End Do
+      Deallocate (Vold, Vnew, P, mat)
+End Program CSR
