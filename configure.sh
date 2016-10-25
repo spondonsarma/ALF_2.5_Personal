f7e94dae8dcb1947a78f271291b6c850877a1eb6
Florian Goth <CaptainSifff@gmx.de>
2016-10-25 16:16:43 +0200

    separate Optimization and other flags in Fakher's build system.

--- configure.sh
+++ configure.sh
@@ -1,5 +1,7 @@
 export DIR=`pwd`
 export f90="gfortran"
-export FL="-c -O3"
+export F90OPTFLAGS="-O3 -march=broadwell"
+export FL="-c ${F90OPTFLAGS}"
 export Libs=${DIR}"/Libraries/"
 export LIB_BLAS_LAPACK="-llapack -lblas"
+#export LIB_BLAS_LAPACK="-Wl,--start-group /opt/intel/compilers_and_libraries_2017.0.098/linux/mkl/lib/intel64_lin/libmkl_gf_lp64.a /opt/intel/compilers_and_libraries_2017/linux/mkl/lib/intel64_lin/libmkl_core.a /opt/intel/compilers_and_libraries_2017/linux/mkl/lib/intel64_lin/libmkl_sequential.a -Wl,--end-group -lpthread -lm -ldl"
