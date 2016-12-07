96a8b8e37ab2300d1a14d607dce339fab28a5338
Florian Goth <CaptainSifff@gmx.de>
2016-12-07 12:04:45 +0100

    make configure.sh more backwards compatible.

--- configure.sh
+++ configure.sh
@@ -1,5 +1,10 @@
-export DIR=`pwd`
-export f90="gfortran"
-export FL="-c -O3"
-export Libs=${DIR}"/Libraries/"
-export LIB_BLAS_LAPACK="-llapack -lblas"
+DIR=`pwd`
+export DIR
+f90="gfortran"
+export f90
+FL="-c -O3"
+export FL
+Libs=${DIR}"/Libraries/"
+export Libs
+LIB_BLAS_LAPACK="-llapack -lblas"
+export LIB_BLAS_LAPACK
