b5bd13abd16f38cc70dbc05acb7cf232c7094a78
Florian Goth <CaptainSifff@gmx.de>
2016-12-09 12:18:31 +0100

    separate Programm configuration and compilation/linking.

--- configure.sh
+++ configure.sh
@@ -1,8 +1,10 @@
+# setting QRREF has the highest priority. Setting nothing selects System lapack for the QR decomposition
+PROGRAMMCONFIGURATION="-DQRREF"
 DIR=`pwd`
 export DIR
 f90="gfortran"
 export f90
-FL="-c -O3"
+FL="-c -O3 ${PROGRAMMCONFIGURATION}"
 export FL
 Libs=${DIR}"/Libraries/"
 export Libs
