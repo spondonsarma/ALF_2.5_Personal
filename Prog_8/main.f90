7b3d3daf71490ed3484c0849388f38b77d046585
Florian Goth <CaptainSifff@gmx.de>
2016-12-15 19:16:05 +0100

    first license variants

--- Prog_8/main.f90
+++ Prog_8/main.f90
@@ -1,3 +1,34 @@
+!  Copyright (C) 2016 The ALF project
+! 
+!     The ALF project is free software: you can redistribute it and/or modify
+!     it under the terms of the GNU General Public License as published by
+!     the Free Software Foundation, either version 3 of the License, or
+!     (at your option) any later version.
+! 
+!     The ALF project is distributed in the hope that it will be useful,
+!     but WITHOUT ANY WARRANTY; without even the implied warranty of
+!     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!     GNU General Public License for more details.
+! 
+!     You should have received a copy of the GNU General Public License
+!     along with Foobar.  If not, see http://www.gnu.org/licenses/.
+!     
+!     Under Section 7 of GPL version 3 we require you to fulfill the following additional terms:
+!     
+!     - It is our hope that this program makes a contribution to the scientific community. Being
+!       part of that community we feel that it is reasonable to require you to give an attribution
+!       back to the original authors if you have benefitted from this program.
+!       Guidelines for a proper citation can be found on the project's homepage
+!       http://alf.physik.uni-wuerzburg.de .
+!       
+!     - We require the preservation of the above copyright notice and this license in all original files.
+!     
+!     - We prohibit the misrepresentation of the origin of the original source files. To obtain 
+!       the original source files please visit the homepage http://alf.physik.uni-wuerzburg.de .
+! 
+!     - If you make substantial changes to the program we require you to either consider contributing
+!       to the ALF project or to mark your material in a reasonable way as different from the original version.
+
 Program Main
 
   Use Operator_mod
@@ -7,7 +38,6 @@ Program Main
   Use Control
   Use Tau_m_mod
   Use Hop_mod
-  
  
   Implicit none
 #ifdef MPI
@@ -91,7 +121,9 @@ Program Main
   Logical        :: prog_truncation
   Real(kind=8)   :: time_bin_start,time_bin_end
     
-  
+  write (*,*) "ALF Copyright (C) 2016 The ALF project contributors"
+  write (*,*) "This Program comes with ABSOLUTELY NO WARRANTY; for details see license.GPL"
+  write (*,*) "This is free software, and you are welcome to redistribute it under certain conditions."
 #ifdef MPI
   Integer        ::  Isize, Irank
   INTEGER        :: STATUS(MPI_STATUS_SIZE)
