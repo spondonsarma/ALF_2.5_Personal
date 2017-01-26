5825e6d205f5bec22beaee544e9b33aa5d1779f9
Florian Goth <florian.goth@physik.uni-wuerzburg.de>
2017-01-26 15:33:04 +0100

    Introduce USEFULFLAGS. This is intended to collect those compiler specific flags like -cpp -std=f2003...

--- set_env.sh
+++ set_env.sh
@@ -5,11 +5,13 @@
 PROGRAMMCONFIGURATION="-DQRREF -DSTAB1"
 PROGRAMMCONFIGURATION=""
 f90="ifort"
-f90="gfortran"
+f90="/opt/pgi/linux86-64/2016/bin/pgfortran"
 export f90
 F90OPTFLAGS="-O3 "
 F90OPTFLAGS="-O3 -Wconversion  -fcheck=all"
-F90OPTFLAGS="-O3 -Wconversion "
+F90OPTFLAGS="-O3 -Minform=inform"
+F90USEFULFLAGS="-Mpreprocess -Minform=inform"
+export F90USEFULFLAGS
 export F90OPTFLAGS
 FL="-c ${F90OPTFLAGS} ${PROGRAMMCONFIGURATION}"
 export FL
@@ -17,5 +19,5 @@ DIR=`pwd`
 export DIR
 Libs=${DIR}"/Libraries/"
 export Libs
-LIB_BLAS_LAPACK="-llapack -lblas"
+LIB_BLAS_LAPACK="-L/opt/pgi/linux86-64/2016/lib -llapack -lblas"
 export LIB_BLAS_LAPACK
