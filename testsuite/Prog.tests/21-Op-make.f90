82b5d04dba45c91ae5c31c894b1e22b955e95cbd
Florian Goth <CaptainSifff@gmx.de>
2017-07-08 17:21:37 +0200

    add a test for Op-make

--- testsuite/Prog.tests/21-Op-make.f90
+++ testsuite/Prog.tests/21-Op-make.f90
@@ -1,5 +1,5 @@
 ! compile with
-! gfortran -Wall -std=f2003 -I ../../../Prog/  -I ../../../Libraries/Modules/ -L ../../../Libraries/Modules/ 21-Op-make.f90 ../../../Prog/Operator.o ../../../Libraries/Modules/modules_90.a -llapack -lblas
+! gfortran -Wall -std=f2003 -I ../../Prog/  -I ../../Libraries/Modules/ -L ../../Libraries/Modules/ 21-Op-make.f90 ../../Prog/Operator.o ../../Libraries/Modules/modules_90.a -llapack -lblas
 !
 Program TESTOPMAKE
 !
@@ -15,21 +15,14 @@ Program TESTOPMAKE
          End Subroutine
       End Interface
 !
-      Complex (Kind=Kind(0.D0)) :: Zre, Zim
-      Complex (Kind=Kind(0.D0)), Dimension (:, :), Allocatable :: VH, &
-     & matnew, matold
-      Complex (Kind=Kind(0.D0)), Dimension (:), Allocatable :: Expop, &
-     & ExpMop
-      Integer :: i, n, j, Ndim, opn
+      Real (Kind=Kind(0.D0)) :: Zre, Zim
+      Complex (Kind=Kind(0.D0)), Dimension (:, :), Allocatable :: matnew, matold
+      Integer :: i, n, j,  opn
       Type (Operator) :: Op
 !
-! setup some test data
-      Ndim = 5
-!
-      Do opn = 1, 8
-      Do Ndim = opn+1, 15
-            Allocate (VH(opn, Ndim), matold(Ndim, Ndim), matnew(Ndim, &
-           & Ndim), Expop(opn), ExpMop(opn))
+      Do opn = 1, 16 ! overflow occurs for our particular O
+            Allocate (matold(opn, opn), matnew(opn, opn))
+           ! setup some test data
             Call Op_seths ()
             Call Op_make (Op, opn)
 !
@@ -50,30 +43,28 @@ Program TESTOPMAKE
             Call Op_exp (Op%g, Op, matnew)
 !
 !
-            Do i = 1, Ndim
-               Do j = 1, Ndim
+            Do i = 1, opn
+               Do j = 1, opn
                   Zre = real (matnew(i, j)-matold(i, j))
                   Zim = aimag (matnew(i, j)-matold(i, j))
                   If (Abs(Zre) > Max(Abs(real(matnew(i, j))), &
-                 & Abs(real(matold(i, j))))*1D-14) Then
+                 & Abs(real(matold(i, j))))*1D-15) Then
                      Write (*,*) "opn: ", opn
                      Write (*,*) "ERROR in real part", real (matnew(i, &
                     & j)), real (matold(i, j)), Zre
 !                     Stop 2
                   End If
                   If (Abs(Zim) > Max(Abs(aimag(matnew(i, j))), &
-                 & Abs(aimag(matold(i, j))))*1D-14) Then
+                 & Abs(aimag(matold(i, j))))*1D-15) Then
                      Write (*,*) "opn: ", opn
                      Write (*,*) "ERROR in imag part", aimag (matnew(i, &
                     & j)), aimag (matold(i, j)), Zim
 !                     Stop 3
                   End If
-               End Do
             End Do
-!
-            Deallocate (VH, matnew, matold, Expop, ExpMop)
-            call Op_clear(Op, opn)
          End Do
+        Deallocate (matnew, matold)
+        call Op_clear(Op, opn)
       End Do
 
 End Program TESTOPMAKE
