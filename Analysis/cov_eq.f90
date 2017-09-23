699b5da687270b84ea692c3e566e27979a099c42
Fakher Assaad <assaad@physik.uni-wuerzburg.de>
2017-09-23 17:30:46 +0200

    Adapted Analysis

--- Analysis/cov_eq.f90
+++ Analysis/cov_eq.f90
@@ -62,18 +62,19 @@
          Real (Kind=Kind(0.d0)) :: Pi, a1_p(2), a2_p(2), L1_p(2), L2_p(2), del_p(2)
          Real (Kind=Kind(0.d0)), allocatable :: AutoCorr(:),En(:)
 
-         Integer             :: L1, L2, I, N_auto
+         Integer             :: L1, L2, I, N_auto, N_SUN
          Character (len=64)  :: Model, Lattice_type
          Type (Lattice)      :: Latt
          Character (len=64) :: File_out
          
 
-         NAMELIST /VAR_lattice/  L1, L2, Lattice_type, Model
+         NAMELIST /VAR_lattice/  L1, L2, Lattice_type, Model, N_SUN
          NAMELIST /VAR_errors/   n_skip, N_rebin, N_Cov, N_Back, N_auto
 
 
 
-         
+
+         N_SUN  = 1
          N_Back = 1
          N_auto = 0
          OPEN(UNIT=5,FILE='parameters',STATUS='old',ACTION='read',IOSTAT=ierr)
@@ -112,6 +113,13 @@
                enddo
             enddo
             close(10)
+         elseif ( Lattice_type == "Pi_Flux" ) then 
+             a1_p(1) =  1.D0   ; a1_p(2) =   1.d0
+             a2_p(1) =  1.D0   ; a2_p(2) =  -1.d0
+             !del_p   =  (a2_p - 0.5*a1_p ) * 2.0/3.0
+             L1_p    =  dble(L1) * (a1_p - a2_p)/2.d0
+             L2_p    =  dble(L2) * (a1_p + a2_p)/2.d0
+             Call Make_Lattice( L1_p, L2_p, a1_p,  a2_p, Latt )
          else
             Write(6,*) "Lattice not yet implemented!"
             Stop
