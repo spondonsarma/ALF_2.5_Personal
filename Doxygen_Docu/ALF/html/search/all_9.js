6f4bd2bf07422bd093346fece36242ec9e69bbd2
Fakher Assaad <assaad@physik.uni-wuerzburg.de>
2018-09-23 13:57:48 +0200

    Updated Doxygen Docu. Updated  LRC.tex to include definition of the Long range Coulomb

--- Doxygen_Docu/ALF/html/search/all_9.js
+++ Doxygen_Docu/ALF/html/search/all_9.js
@@ -13,6 +13,7 @@ var searchData=
   ['lrc_5fdraw_5ffield',['lrc_draw_field',['../namespacelrc__mod.html#a7be87f29295af7d21e31a0a1041a1b89',1,'lrc_mod']]],
   ['lrc_5fmod',['lrc_mod',['../namespacelrc__mod.html',1,'']]],
   ['lrc_5fmod_2ef90',['LRC_mod.f90',['../LRC__mod_8f90.html',1,'']]],
+  ['lrc_5fprint',['lrc_print',['../namespacelrc__mod.html#a1b108f23136300a5a1fdb9acda9ce319',1,'lrc_mod']]],
   ['lrc_5fs0',['lrc_s0',['../namespacelrc__mod.html#a2fced9bec185cfc8defb4b9f36fef9b3',1,'lrc_mod']]],
   ['lrc_5fset_5fvij',['lrc_set_vij',['../namespacelrc__mod.html#a9c82942df4db5d5acfbe9447b66a7c5c',1,'lrc_mod']]],
   ['lrc_5fv_5ffunc',['lrc_v_func',['../namespacelrc__mod.html#ad072c5b08156a299392531ec8ef63f6a',1,'lrc_mod']]],
--- Doxygen_Docu/ALF/html/search/functions_8.js
+++ Doxygen_Docu/ALF/html/search/functions_8.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['lrc_5fclear',['lrc_clear',['../namespacelrc__mod.html#abc59c48a9340669b00b7340797b0f012',1,'lrc_mod']]],
   ['lrc_5fdraw_5ffield',['lrc_draw_field',['../namespacelrc__mod.html#a7be87f29295af7d21e31a0a1041a1b89',1,'lrc_mod']]],
+  ['lrc_5fprint',['lrc_print',['../namespacelrc__mod.html#a1b108f23136300a5a1fdb9acda9ce319',1,'lrc_mod']]],
   ['lrc_5fs0',['lrc_s0',['../namespacelrc__mod.html#a2fced9bec185cfc8defb4b9f36fef9b3',1,'lrc_mod']]],
   ['lrc_5fset_5fvij',['lrc_set_vij',['../namespacelrc__mod.html#a9c82942df4db5d5acfbe9447b66a7c5c',1,'lrc_mod']]],
   ['lrc_5fv_5ffunc',['lrc_v_func',['../namespacelrc__mod.html#ad072c5b08156a299392531ec8ef63f6a',1,'lrc_mod']]],
