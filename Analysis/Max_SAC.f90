7d470bc85a81f7b11352e403b783abc6d30210b7
Fakher Assaad Home <assaad@physik.uni-wuerzburg.de>
2019-09-01 22:38:13 +0200

    update doc and Max_SAC.f90

--- Analysis/Max_SAC.f90
+++ Analysis/Max_SAC.f90
@@ -1,5 +1,45 @@
-     Program Test
+!  Copyright (C) 2016-2019 The ALF project
+! 
+!     The ALF project is free software: you can redistribute it and/or modify
+!     it under the terms of the GNU General Public License as published by
+!     the Free Software Foundation, either version 3 of the License, or
+!     (at your option) any later version.
+! 
+!     The ALF project is distributed in the hope that it will be useful,
+!     but WITHOUT ANY WARRANTY; without even the implied warranty of
+!     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!     GNU General Public License for more details.
+! 
+!     You should have received a copy of the GNU General Public License
+!     along with Foobar.  If not, see http://www.gnu.org/licenses/.
+!     
+!     Under Section 7 of GPL version 3 we require you to fulfill the following additional terms:
+!     
+!     - It is our hope that this program makes a contribution to the scientific community. Being
+!       part of that community we feel that it is reasonable to require you to give an attribution
+!       back to the original authors if you have benefitted from this program.
+!       Guidelines for a proper citation can be found on the project's homepage
+!       http://alf.physik.uni-wuerzburg.de .
+!       
+!     - We require the preservation of the above copyright notice and this license in all original files.
+!     
+!     - We prohibit the misrepresentation of the origin of the original source files. To obtain 
+!       the original source files please visit the homepage http://alf.physik.uni-wuerzburg.de .
+! 
+!     - If you make substantial changes to the program we require you to either consider contributing
+!       to the ALF project or to mark your material in a reasonable way as different from the original version.
+     Program MaxEnt_Wrapper
 
+!--------------------------------------------------------------------
+!> @author 
+!> ALF-project
+!
+!> @brief 
+!> General wrapper for maxent.  Handles particle, partile-hole, particle-particle,
+!> channels as well as zero temperature.  See documentation for details.       
+!> 
+!
+!--------------------------------------------------------------------
        Use MaxEnt_stoch_mod
 
        Implicit  None
@@ -253,7 +293,7 @@
        close(43)
        
        
-     end Program Test
+     end Program MaxEnt_Wrapper
      
 
 
