ea7b0f9405cc3cd938c8632054bbaa212a70a392
Fakher Assaad Home <assaad@physik.uni-wuerzburg.de>
2019-09-01 16:11:45 +0200

    Moved Max_SAC_ph.f90 to Max_SAC.f90 deleted Max_SAC_ph.f90

--- Analysis/Max_SAC.f90
+++ Analysis/Max_SAC.f90
@@ -1,69 +1,208 @@
      Program Test
 
        Use MaxEnt_stoch_mod
-       use Files_mod
 
-       Implicit Real (Kind=Kind(0.d0)) (A-G,O-Z)
-       Implicit Integer (H-N)
+       Implicit  None
+       !Implicit Real (Kind=Kind(0.d0)) (A-G,O-Z)
+       !Implicit Integer (H-N)
        
-       Real (Kind=Kind(0.d0)), Dimension(:), allocatable :: XQMC, XTAU, Alpha_tot, xom, A, &
-            &    ERROR, ARES, XQMC_ST
-       Real (Kind=Kind(0.d0)), Dimension(:,:), allocatable :: XCOV, Cov_st
-       Real (Kind=Kind(0.d0)), External :: XKER, Back_trans_Aom, F_Fit
-       Character (len=64) :: File1, File2
-       Character (len=1)  :: Fermion_type
-       Complex (Kind=Kind(0.d0)) :: Z
-       Character (Len=64) :: command
+       Real (Kind=Kind(0.d0)), Dimension(:)  , allocatable :: XQMC, XQMC_st, XTAU, Xtau_st, &
+            &                                                 Alpha_tot, om_bf, alp_bf, xom, A
+       Real (Kind=Kind(0.d0)), Dimension(:,:), allocatable :: XCOV, XCOV_st
+       Real (Kind=Kind(0.d0))                              :: X_moments(2), Xerr_moments(2)
+       Real (Kind=Kind(0.d0)), External                    :: XKER_ph, Back_trans_ph, XKER_pp, Back_trans_pp, &
+            &                                                 XKER_p, Back_trans_p
+       Character (Len=64)                                  :: command, File1, File2
+       Complex (Kind=Kind(0.d0))                           :: Z
        
 
-
+       Integer                :: Ngamma, Ndis,  NBins, NSweeps, Nwarm, N_alpha, L_cov
+       Real (Kind=Kind(0.d0)) :: OM_st, OM_en,  alpha_st, R
+       Logical                :: Checkpoint
+       Character (Len=2)      :: Channel
        
+       Integer                :: nt, nt1, io_error, n,nw, nwp, ntau, N_alpha_1, i
+       Integer                :: ntau_st, ntau_en, ntau_new
+       Real (Kind=Kind(0.d0)) :: dtau, pi, xmom1, x,x1,x2, tau, omp, om, Beta,err, delta, Dom
+       Real (Kind=Kind(0.d0)) :: Zero
+
+       NAMELIST /VAR_Max_Stoch/ Ngamma, Ndis,  NBins, NSweeps, Nwarm, N_alpha, L_cov, &
+            &                   OM_st, OM_en,  alpha_st, R,  Checkpoint, Channel
+
        open(unit=30,file='paramSAC',status='old',action='read', iostat=io_error) 
-       Open(unit=50,File='Info',Status="unknown")
        if (io_error.eq.0) then
-          write(50,*) 'Reading in from paramSAC'
-          read(30,*)  Ngamma, OM_st, OM_en, Ndis,  NBins, NSweeps, Nwarm
-          read(30,*)  N_alpha, alpha_st, R
+          READ(30,NML=VAR_Max_Stoch)
        else
-          write(6,*) 'No file paramSAC! ' 
+          write(6,*) 'No file paramSAC ' 
           stop
        endif
-       close(30)
+
+       Open(unit=50,File='Info_MaxEnt',Status="unknown")
+       write(50,*) 'Channel      :: ', Channel
+       If (Channel == "ph" )  then
+          Write(50,*)  'Om_start is set to zero. PH channel corresponds to symmetric data '
+          Om_st = 0.d0
+       endif
+       Write(50,*) 'Covariance         :: ', L_cov
+       Write(50,*) 'Checkpoint         :: ', Checkpoint
+       Write(50,*) 'Om_st, Om_en       :: ', Om_st, Om_en
+       Write(50,*) 'Delta Om           :: ', (Om_en - Om_st)/real(Ndis,kind(0.d0))
+       Write(50,*) 'Bins, Sweeps, Warm :: ', NBins, NSweeps, Nwarm
+       If (N_alpha <= 10 ) then
+          Write(50,*) 'Not enough temperatures: N_alpha has to be bigger than 10'
+          Stop
+       Endif
+       Write(50,*) 'N_Alpha, Alpha_st,R:: ', N_alpha, alpha_st, R
        
 
        open (unit=10,File="g_dat", status="unknown") 
-       read(10,*) ntau 
-       Allocate ( XCOV(NTAU,NTAU), XQMC(NTAU), XQMC_ST(NTAU), XTAU(NTAU) )
-       xcov = 0.d0
-       do nt = 1,ntau
-          read(10,*) Xtau(nt), Xqmc(nt), err
-          if (nt == 1 .or. nt == ntau) then  
-             if (err < 10.0D-3) err = 10.0D-3      
-          endif
+       read(10,*)  ntau, n, Beta
+       Allocate ( XCOV(NTAU,NTAU), XQMC(NTAU),XTAU(NTAU) )
+       XCOV  = 0.d0
+       Do nt = 1,NTAU
+          read(10,*)  xtau(nt), xqmc(nt), err 
           xcov(nt,nt) = err*err
-       enddo
+       Enddo
+       if (L_cov.eq.1) then
+          do nt = 1,ntau
+             do nt1 = 1,ntau
+                read(10,*) xcov(nt,nt1)
+             enddo
+          enddo
+       endif
        close(10)
-       xqmc_st = xqmc
        dtau = Xtau(2) - Xtau(1)
-       Beta = Xtau(Ntau)
-          
-       xmom1 = xqmc(1) + xqmc(Ntau)
+
+       Zero = 1.D-10
+       pi = acos(-1.d0)
+       Select Case (Channel)
+       Case ("PH")
+          xmom1 = pi * xqmc(1) 
+       Case ("PP")
+          xmom1 = 2.d0* pi * xqmc(1)
+       Case ("P")
+          xmom1 =  pi
+          !  Remove the tau = beta point from the data since  it is correlated
+          !  due to the sum rule with  the tau=0 data point. Also if the tau = 0
+          !  data point has no fluctations (due to particle-hole symmetry for instance)
+          !  it will be removed.
+          Ntau_en = Ntau - 1
+          Ntau_st = 1
+          if ( xcov(1,1) < zero )  ntau_st = 2
+          ntau_new  =  ntau_en - Ntau_st   + 1
+          if (Ntau_new .ne. NTAU ) then 
+             Allocate ( XCOV_st(NTAU_new,NTAU_new), XQMC_st(NTAU_new),XTAU_st(NTAU_new) )
+             do nt = 1,ntau_new
+                XQMC_st(nt) = XQMC(nt + ntau_st -1 )
+                XTAU_st(nt) = XTAU(nt + ntau_st -1 )
+             enddo
+             do nt = 1,ntau_new
+                do nt1 = 1,ntau_new
+                   Xcov_st(nt,nt1) = Xcov(nt + ntau_st -1, nt1 + ntau_st -1)
+                enddo
+             enddo
+             NTAU = NTAU_New
+             Deallocate (XCOV, XQMC,XTAU )
+             allocate   (XCOV(NTAU,NTAU), XQMC(NTAU),XTAU(NTAU) )
+             XCOV = XCOV_st
+             XQMC = XQMC_st
+             XTAU = XTAU_st
+             Deallocate (XCOV_st, XQMC_st,XTAU_st )
+          endif
+       Case default 
+          Write(6,*) "Channel not yet implemented"
+          Stop
+       end Select
+       ! Store
+       Allocate ( XCOV_st(NTAU,NTAU), XQMC_st(NTAU),XTAU_st(NTAU) )
+       XCOV_st = XCOV
+       XQMC_st = XQMC
+       XTAU_st = XTAU
+       
+       
        Allocate (Alpha_tot(N_alpha) )
        do nt = 1,N_alpha
           alpha_tot(nt) = alpha_st*(R**(nt-1))
        enddo
        write(50,*) 'First Moment, Beta  ', Xmom1, Beta
        close(50)
-          
-          
-       Call MaxEnt_stoch(XQMC, Xtau, Xcov, Xmom1, XKER, Back_Trans_Aom, Beta, &
-            &            Alpha_tot, Ngamma, OM_ST, OM_EN, Ndis, Nsweeps, NBins, NWarm) 
+
+       Select Case (Channel)
+       Case ("PH")
+          If (L_cov == 1 ) then
+             Call MaxEnt_stoch(XQMC, Xtau, Xcov, Xmom1, XKER_ph, Back_Trans_ph, Beta, &
+                  &            Alpha_tot, Ngamma, OM_ST, OM_EN, Ndis, Nsweeps, NBins, NWarm, L_Cov)
+          else
+             Call MaxEnt_stoch(XQMC, Xtau, Xcov, Xmom1, XKER_ph, Back_Trans_ph, Beta, &
+                  &            Alpha_tot, Ngamma, OM_ST, OM_EN, Ndis, Nsweeps, NBins, NWarm)
+          endif
+          ! Beware: Xqmc and cov are modified in the MaxEnt_stoch call.
+       Case ("PP")
+          If (L_cov == 1 ) then
+             Call MaxEnt_stoch(XQMC, Xtau, Xcov, Xmom1, XKER_pp, Back_Trans_pp, Beta, &
+                  &            Alpha_tot, Ngamma, OM_ST, OM_EN, Ndis, Nsweeps, NBins, NWarm, L_Cov)
+          else
+             Call MaxEnt_stoch(XQMC, Xtau, Xcov, Xmom1, XKER_pp, Back_Trans_pp, Beta, &
+                  &            Alpha_tot, Ngamma, OM_ST, OM_EN, Ndis, Nsweeps, NBins, NWarm)
+          endif
+          ! Beware: Xqmc and cov are modified in the MaxEnt_stoch call.
+       Case ("P")
+          If (L_cov == 1 ) then
+             Call MaxEnt_stoch(XQMC, Xtau, Xcov, Xmom1, XKER_p, Back_Trans_p, Beta, &
+                  &            Alpha_tot, Ngamma, OM_ST, OM_EN, Ndis, Nsweeps, NBins, NWarm, L_Cov)
+          else
+             Call MaxEnt_stoch(XQMC, Xtau, Xcov, Xmom1, XKER_p, Back_Trans_p, Beta, &
+                  &            Alpha_tot, Ngamma, OM_ST, OM_EN, Ndis, Nsweeps, NBins, NWarm)
+          endif
+          ! Beware: Xqmc and cov are modified in the MaxEnt_stoch call.
+       Case default 
+          Write(6,*) "Channel not yet implemented"
+          Stop
+       end Select
        
-       
+       If ( .not.  Checkpoint ) then
+          Command = "rm dump*"
+          Call System (Command)
+          Command = "ls"
+          Call System (Command)
+       endif
+
+       Open (Unit = 10,File="Best_fit", Status ="unknown") 
+       Allocate (om_bf(Ngamma), alp_bf(Ngamma) )
+       DO i = 1, Ngamma
+          read(10,*)  om_bf(i), alp_bf(i)
+       Enddo
+       close(10) 
+
+       Open (Unit = 11,File="Data_out", Status ="unknown") 
+       do nt = 1,Ntau
+          X = 0.d0
+          tau = xtau_st(nt)
+          Select Case (Channel)
+          Case ("PH")
+             do i = 1,Ngamma
+                X = X + alp_bf(i)*Xker_ph(tau,om_bf(i), beta)
+             enddo
+          Case ("PP")
+             do i = 1,Ngamma
+                X = X + alp_bf(i)*Xker_pp(tau,om_bf(i), beta)
+             enddo
+          Case ("P")
+             do i = 1,Ngamma
+                X = X + alp_bf(i)*Xker_p(tau,om_bf(i), beta)
+             enddo
+          Case default 
+             Write(6,*) "Channel not yet implemented"
+             Stop
+          end Select
+          Write(11,"(F14.7,2x,F14.7,2x,F14.7,2x,F14.7)")  xtau_st(nt), xqmc_st(nt),  sqrt(xcov_st(nt,nt)), xmom1*X
+       enddo
+       close(11)
+
        N_alpha_1 = N_alpha - 10  
        File1 ="Aom_ps"
        file2 =File_i(File1,N_alpha_1)
-       Write(50,*) file2
+       !Write(50,*) file2
        Open(Unit=66,File=file2,status="unknown")
        Allocate (xom(Ndis), A(Ndis))
        do nw = 1,Ndis
@@ -72,9 +211,8 @@
        close(66)
        Dom = xom(2) - xom(1)
        
-       
        ! Compute the real frequency Green function.
-       delta = 4.0*Dom
+       delta = Dom
        Open (Unit=43,File="Green", Status="unknown", action="write")
        pi = acos(-1.d0)
        do nw = 1,Ndis
@@ -85,52 +223,83 @@
              Z = Z + A(nwp)/cmplx( om -  omp, delta, kind(0.d0))
           enddo
           Z = Z * dom
-          write(43,"(F14.7,2x,F16.8,2x,F16.8)" ) xom(nw), dble(Z), -Aimag(Z)/pi
+          write(43,"('X'2x,F14.7,2x,F16.8,2x,F16.8)" ) xom(nw), dble(Z), -Aimag(Z)/pi
        enddo
        close(43)
        
        
-       Open (Unit=70,File="data_out", status="unknown")
-       do nt = 1,Ntau
-          X = 0.d0
-          do  nw = 1, Ndis
-             X = X + Xker(Xtau(nt),xom(nw), beta)*A(nw)
-          enddo
-          X = X *dom
-          write(70,2005) xtau(nt), xqmc_st(nt), sqrt(xcov(nt,nt)), X
-       enddo
-2005   format(F14.7,2x,F14.7,2x,F14.7,2x,F14.7)
-       Close(70)
-       
-       Command = "rm dump*"
-       Call System (Command)
-       Command = "ls"
-       Call System (Command)
-       
-          
      end Program Test
      
-     
-     
-     Real (Kind=Kind(0.d0)) function XKER(tau,om, beta)
-       
+
+
+     Real (Kind=Kind(0.d0)) function XKER_ph(tau,om, beta)
+
        Implicit None
        real (Kind=Kind(0.d0)) :: tau, om, pi, beta
 
-       !pi = 3.1415927
-       XKER = exp(-tau*om) / ( 1.d0 + exp(-Beta*om) ) ! /pi
-       !XKER = exp(-tau*om)  ! / ( 1.d0 + exp(-Beta*om) ) ! /pi
+       pi = 3.1415927
 
-     end function XKER
+       XKER_ph = (exp(-tau*om) + exp(-( beta - tau )*om ) )/( pi*(1.d0 + exp( - beta * om ) ) )
 
-     Real (Kind=Kind(0.d0)) function Back_trans_Aom(Aom, om, beta)
+     end function XKER_ph
+
+     Real (Kind=Kind(0.d0)) function XKER_pp(tau,om, beta)
+
+       Implicit None
+       real (Kind=Kind(0.d0)) :: tau, om, pi, beta
+
+       pi = 3.1415927
+
+       XKER_pp = exp(-tau*om) / ( pi*(1.d0 + exp( - beta * om ) ) )
+
+     end function XKER_pp
+
+     Real (Kind=Kind(0.d0)) function XKER_p(tau,om, beta)
+
+       Implicit None
+       real (Kind=Kind(0.d0)) :: tau, om, pi, beta
+
+       pi = 3.1415927
+
+       XKER_p  = exp(-tau*om) / ( pi*(1.d0 + exp( - beta * om ) ) )
+
+     end function XKER_p
+
+     
+     Real (Kind=Kind(0.d0)) function Back_trans_ph(Aom, om, beta)
 
        Implicit None
        real (Kind=Kind(0.d0)) ::  Aom, om, beta
 
-       Back_trans_Aom = Aom 
+       Back_trans_ph = Aom/(1.d0 + exp(-beta*om) )   
+       ! This gives S(q,om) = chi(q,om)/(1 - e^(-beta om))
 
-     end function BACK_TRANS_AOM
+     end function BACK_TRANS_PH
 
-     
+     Real (Kind=Kind(0.d0)) function Back_trans_pp(Aom, om, beta)
+            
+       Implicit None
+       real (Kind=Kind(0.d0)) ::  Aom, om, beta
+       real (Kind=Kind(0.d0)) :: Zero
+
+       Zero = 1.D-8
+
+       if ( abs(om) < zero ) then
+          Back_trans_pp = beta * Aom/2.d0
+       else
+          Back_trans_pp = Aom * (1.d0 + exp(-beta*om) ) / (om *( 1.d0 + exp(-beta*om) ) )
+       endif
+       ! This gives  = chi(q,om)/omega
+
+     end function BACK_TRANS_PP
+
+     Real (Kind=Kind(0.d0)) function Back_trans_p(Aom, om, beta)
+            
+       Implicit None
+       real (Kind=Kind(0.d0)) ::  Aom, om, beta
+
+       Back_trans_p =  Aom
+       
+     end function BACK_TRANS_P
+
      
