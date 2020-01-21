96ba5e55cfccc48458136ed68fd794b946bd93fd
Jefferson Stafusa Elias Portela <jstafusa@physik.uni-wuerzburg.de>
2020-01-21 14:31:46 +0100

    Putting some order.

--- testsuite/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/stafusa/ALF/ALF/testsuite
-# Build directory: /home/stafusa/ALF/ALF/testsuite/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("matmod.tests")
-subdirs("Prog.tests")
