7428bf3c236ac62d910a09b4101174c10c35eac1
Debian <debian@localhost>
2020-08-29 16:34:32 +0200

    Add data for Kondo Figure

--- Documentation/Figures/Kondo/Kondo.py
+++ Documentation/Figures/Kondo/Kondo.py
@@ -9,31 +9,35 @@ Created on Sat Aug 29 05:20:44 2020
 from py_alf import Simulation            # Interface with ALF
 import numpy as np                       # Numerical library
 sims = []                                # Vector of Simulation instances
-for Ham_Uf in [0.0, 0.1, 0.2]:           # Values of dtau
+#for Ham_Uf in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4,
+for Ham_Uf in [1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0]:           # Values of Uf
     print(Ham_Uf)
     sim_dict = {"Model": "Kondo", 
                 "Lattice_type": "Bilayer_square", 
                 "L1": 4 , "L2": 4, 
                 "Ham_Uf": Ham_Uf,  
                 "Beta": 5.0, 
-                "Nsweep": 100, 
-                "NBin": 5}
+                "Nsweep": 500, 
+                "NBin": 400,
+                "Ltau": 0}
     sim = Simulation('Kondo', sim_dict,
-                     alf_dir = '/Users/fassaad/Programs/ALF/Work/',
-                     branch = '151-introduce-sun-kondo-hamiltonian-for-bilayer-lattices')
+                     alf_dir = '/home/debian/Work/',
+                     branch = '151-introduce-sun-kondo-hamiltonian-for-bilayer-lattices',
+                     machine= 'Intel',
+                     mpi    = True,
+                     n_mpi  = 12)
     sims.append(sim)
 #sims[0].compile(target = "Kondo")
 Con = np.empty((len(sims), 2))          # Matrix for storing energy values
 Uf  = np.empty((len(sims),))           # Matrix for Dtau values, for plotting
 for i, sim in enumerate(sims):
-    #sim.run()
-    #sim.analysis() 
+    print (sim.sim_dir)
+    sim.run()
+    print (sim.sim_dir)
+    sim.analysis() 
     Uf[i] = sim.sim_dict['Ham_Uf']                             # Store Uf value
     Con[i] = sim.get_obs(['Constraint_scalJ'])['Constraint_scalJ']['obs']  # Store constraint
 
-print(Uf)
-print(Con)
-print()
 with open('Constraint.dat', 'w') as file:
     for i in range(len(sims) ):
         file.write('%6.6f\t' % (Uf[i])) 
