4083132cbb6e110f6273f53a517191ccb52f46cb
Francesco Parisen Toldin <francesco.parisentoldin@physik.uni-wuerzburg.de>
2021-06-14 10:22:27 +0200

    Hopping_Matrix_Type%Multiplicity is defined private and computed in Predefined_Hoppings_set_OPT

--- Prog/Hamiltonians/Hamiltonian_Hubbard_smod.F90
+++ Prog/Hamiltonians/Hamiltonian_Hubbard_smod.F90
@@ -417,7 +417,7 @@
                   &                                                 List, Invlist, Latt, Latt_unit )
 
           end Select
-
+          
           Call  Predefined_Hoppings_set_OPT(Hopping_Matrix,List,Invlist,Latt,  Latt_unit,  Dtau, Checkerboard, Symm, OP_T )
 
           Deallocate (Ham_T_vec, Ham_T2_vec, Ham_Tperp_vec, Ham_Chem_vec, Phi_X_vec, Phi_Y_vec, &
