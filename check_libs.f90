c1408d242f6d35861404e3bd20773320b0af76cc
Jefferson Stafusa Elias Portela <jstafusa@physik.uni-wuerzburg.de>
2022-03-31 12:10:46 +0200

    Support PGI; minor bug fix.

--- check_libs.f90
+++ check_libs.f90
@@ -1,9 +1,9 @@
 program check_libs
   use iso_fortran_env
-  integer :: n,ix=1,iy=1
-  real(kind=8) :: A(1),B(1)
+  integer :: n=1, ix=1, iy=1
+  real(kind=8) :: A(1), B(1)
   real(kind=8) :: z
-  real(kind=8), external :: ddot
+  real(kind=8), external :: ddot                                                                                                                                                                         
   A(1)=1; B(1)=1
   z=ddot(n,A,ix,B,iy)
   !write(*,*) "Linear algebra libraries found."
