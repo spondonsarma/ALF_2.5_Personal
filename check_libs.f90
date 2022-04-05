6a5ec2ed53cd972e5ef4a14d0c2b3014822c5582
Jonas Schwab <jonas.schwab@physik.uni-wuerzburg.de>
2022-04-06 00:03:58 +0200

    check_libs.f90: Test output of calls

--- check_libs.f90
+++ check_libs.f90
@@ -1,14 +1,25 @@
+! Small check if BLAS and LAPACK libraries are present and working.
 program check_libs
 
   use iso_fortran_env
-  integer :: N=2, ix=1, iy=1,  LDA=2, LWMAX=1000, INFO, LWORK=-1
-  real    (kind=kind(0.d0)) :: C(2), D(2), dot,   W(2), RWORK(4)
-  complex (kind=kind(0.d0)) :: A(2,2), WORK(1000)
+  integer, parameter :: N=2, ix=1, iy=1, LDA=2, LWORK=100
+  integer :: INFO
+  real    (kind=kind(0.d0)) :: C(2), D(2), dot, W(2), RWORK(4)
+  complex (kind=kind(0.d0)) :: A(2,2), WORK(LWORK)
   real    (kind=kind(0.d0)), external :: ddot
-  data    A /(1,0),(0,0),(0,0),(2,0)/,  C /3,1415/, D /42,0/
+  data    A /(0,0),(0,1),(0,-1),(0,0)/,  C /3,1415/, D /42,0/
   external ZHEEV
   
   dot = ddot(n,C,ix,D,iy)
+  if (.not. abs(dot-126d0) < 1d-10) then
+    write(error_unit, *) "Problem with BLAS"
+    error stop
+  endif
+
   call ZHEEV( 'V', 'U', N, A, LDA, W, WORK, LWORK, RWORK, INFO )
+  if ( .not. (abs(W(1)+1d0) < 1d-10 .and. abs(W(1)+1d0) < 1d-10) ) then
+    write(error_unit, *) "Problem with LAPACK"
+    error stop
+  endif
   
 end program check_libs
