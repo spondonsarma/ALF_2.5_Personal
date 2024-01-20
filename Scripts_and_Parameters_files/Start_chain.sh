14738d3651a99e8706fbaf88984a06042146a284
Fakher Assaad <assaad@physik.uni-wuerzburg.de>
2024-01-20 07:42:44 +0100

    Worked on  Start_chain.sh

---
+++ Scripts_and_Parameters_files/Start_chain.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+shopt -s nocasematch 
+Variable=(Run L_Bath Ham_U Ham_Jk  Beta  Dtaux NW NS NB CPU)
+Name=(Run L_Bath_R Ham_U_R Ham_Jk_R  Beta_R Dtau_R NW_R NS_R NB_R CPU_R)
+#      0   1       2       3         4      5      6     7   8    9
+length=${#Variable[@]}
+{
+read -a Variable
+echo "${Variable[@]}"
+echo "${Variable[0]}"
+while [ ! "${Variable[0]}" = "stop" ];   do
+    if [ "${Variable[0]}" = "Y" ]; then
+	
+	string=${Variable[4]}; B_R_dir="${string//\.0/}"
+	echo "B_R_dir"
+	
+	export Dir="L${Variable[1]}_U${Variable[2]}_J${Variable[3]}_B${B_R_dir}_Dt${Variable[5]}"
+	echo "$Dir" 
+	if [ ! -e "$Dir" ]; then
+	    mkdir "$Dir"
+	    cd  "$Dir" || exit
+	    cp  ../Start/* . 
+	    cd ..
+	else
+	    cd "$Dir" || exit
+	    cp ../Start/parameters .
+	    bash out_to_in.sh
+	    cd ..
+	fi
+	cd "$Dir" || exit
+
+	
+	((i=1))
+	while [  $i -lt "$length" ]; do
+	    sed s/"${Name[$i]}"/"${Variable[$i]}"/    parameters  > tmp
+	    mv tmp parameters
+	    ((i=i+1)) 
+	done
+
+	echo "$Dir"
+	echo  "nohup mpirun  -np 6 /home/debian/Kondo_Impurities/Prog/ALF.out & " > run.sh  
+	bash run.sh 
+	
+	cd ..
+    fi
+    read -a Variable
+done
+}<Sims
+
